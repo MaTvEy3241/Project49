85fe7220e17ec0cd4afb54cfb23fa81e47bb68cd
EvgenDev09 <eugene.lippa@gmail.com>
2024-03-14 18:47:02 +0300

    Add project files.

---
+++ Project49/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+class Plane {
+public:
+	string name;
+	int spead;
+	double length;
+	double height;
+	int count;
+
+};
+
+int main() {
+	Plane plane1;
+
+	plane1.name = "Boing 447-400";
+	plane1.spead = 950;
+
+	return 0;
+}
